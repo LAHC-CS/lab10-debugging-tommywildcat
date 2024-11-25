e39349a0346cde7fd07e59d3c8dbefaca2a3a1a0
Tom Margulies <margtc3429@student.laccd.edu>
2024-11-24 19:42:15 -0800

    actual first draft after git headache

--- src/main/java/Exercise_1.java
+++ src/main/java/Exercise_1.java
@@ -1,27 +1,46 @@
 class Exercise_1 {
 
+    /*
+     * We consider a new type of error: DESIGNB error.  Since arr[i] == i for
+     * all value of i, we can write
+     * 
+     * public static void findDivisableBy(int n) {
+     *
+     *     // for every element of the array
+     *     boolean first = true;
+     *     for(int i = 0; i < NUM_ELEMENTS; i++) {
+     *
+     *         // check if element is divisible by n
+     *         if((i % n) == 0) {
+     *             //print if divisibile
+     *             System.out.print((first ? "" : ", ") + i);
+     *             first = false;
+     *         };
+     *     };
+     *     System.out.println("");
+     * }
+     */
+
     //number of elements we want to check for
-    public static int numElements = 100;
+    public static final int NUM_ELEMENTS = 100;
 
     public static void main(String[] args) {
 
         // Check if given a single argument
-        if(args.length == 1) {
+        if(args.length != 1) {
             System.out.println("./Exercise_1 value");
             return; 
         }
 
-        int n = args[0];
+        int n = Integer.parseInt(args[0]);
 
         //create new array of numElements long
-        int[] arr = new int[numElement];
+        int[] arr = new int[NUM_ELEMENTS];
 
-        //populate array from 0 to numElements
-        for(int i = 0; i <= numElements; i++) {
+        //populate array from 0 to NUM_ELEMENTS-1
+        for(int i = 0; i < NUM_ELEMENTS; i++) {
             arr[i] = i;
-        }
-        
-        return;
+        };
 
         //find Divisable by n
         findDivisableBy(arr, n);
@@ -30,14 +49,16 @@ class Exercise_1 {
     public static void findDivisableBy(int arr[], int n) {
 
         // for every element of the array
-        for(int i = 0; i <= numElements; i==i+1) {
+        boolean first = true;
+        for(int i = 0; i < NUM_ELEMENTS; i++) {
 
             // check if element is divisible by n
-            if((arr[i] % n) = 0) {
+            if((arr[i] % n) == 0) {
                 //print if divisibile
-                System.out.print arr[i] + ",")
-            }
-        }
-        System.out.printIn("");
+                System.out.print((first ? "" : ", ") + arr[i]);
+                first = false;
+            };
+        };
+        System.out.println("");
     }
 }
